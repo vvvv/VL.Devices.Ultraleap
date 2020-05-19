91dc6563fa8b57b2390a5362b19fc8a15d3ce26f
joreg <joreg@vvvv.org>
2020-05-19 20:04:13 +0200

    reworked and cleaned up

--- src/LeapWrapper.cs
+++ src/LeapWrapper.cs
@@ -5,52 +5,6 @@ using System.Linq;
 
 namespace LeapOrionVL
 {
-    public class LeapHelper
-    {
-        //we flagged Leap.Frame as an immutable type
-        //However, Leap.Frame.Deserialize returns void
-        //Therefore it cannot be used like the method of an immutable type, so here's the workaround:
-        public static Leap.Frame Deserialize(IEnumerable<byte> data, out String errorMessage, out bool error)
-        {
-            error = false;
-            errorMessage = "";
-            Leap.Frame frame = new Leap.Frame();
-            try
-            {
-                frame.Deserialize(data.ToArray());
-            }
-            catch(Exception e)
-            {
-                error = true;
-                errorMessage = e.ToString();
-            }
-            return frame;
-        }
-
-        //public static Leap.Bone GetBone(Leap.Finger finger, BoneType type)
-        //{
-        //    return finger.Bone((Leap.Bone.BoneType) type);
-        //}
-
-        //public static void GetBones(Leap.Finger finger, out Leap.Bone metacarpal, out Leap.Bone proximal, out Leap.Bone intermediate, out Leap.Bone distal)
-        //{
-        //    metacarpal = finger.Bone((Leap.Bone.BoneType) BoneType.Metacarpal);
-        //    proximal = finger.Bone((Leap.Bone.BoneType) BoneType.Proximal);
-        //    intermediate = finger.Bone((Leap.Bone.BoneType) BoneType.Intermediate);
-        //    distal = finger.Bone((Leap.Bone.BoneType) BoneType.Distal);
-        //}
-
-        //public static FingerType GetFingerType(Leap.Finger finger)
-        //{
-        //    return (FingerType) finger.Type;
-        //}
-
-        //public static BoneType GetBoneType(Leap.Bone bone)
-        //{
-        //    return (BoneType) bone.Type;
-        //}
-    }
-
     public enum FingerType
     {
         //original, from Leap API
@@ -76,11 +30,13 @@ namespace LeapOrionVL
         TYPE_PROXIMAL,
         TYPE_INTERMEDIATE,
         TYPE_DISTAL,
+        TYPE_INVALID,
         */
         Metacarpal,
         Proximal,
         Intermediate,
-        Distal
+        Distal,
+        Invalid
     }
 
     //only used in 'Selectors' to be able to choose from a list (enum)
@@ -96,5 +52,4 @@ namespace LeapOrionVL
         Leftmost,
         Rightmost
     }
-}
- 
+} 
